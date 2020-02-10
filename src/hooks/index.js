85500a334d922eb9cff2acecad9a25505640e673
Alexander Melo <alexandermelox@gmail.com>
2020-02-10 12:00:53 -0600

    removed cloudinary hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -18,16 +18,6 @@ export const useSiteMetadata = () => {
   return data.site.siteMetadata;
 };
 
-// TODO: Add the new images to cloudinary
-export const useCloudinarySVGImages = () => {
-  const images = [];
-  for (let i = 1; i <= 7; i++) {
-    let imgUrl = `https://res.cloudinary.com/alexander-melo-assets/image/upload/v1580320841/Craftcon-assets/images/${i}.svg`;
-    images.push(imgUrl);
-  }
-  return images;
-};
-
 // returns the dispatch function provided by the store context
 export const useDispatch = () => React.useContext(store).dispatch;
 // returns the store state
